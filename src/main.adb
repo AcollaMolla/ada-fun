c1f58c32d0b8de577550b388eee71588ecfec703
Anton Engman <anton.engman@knowit.se>
2020-03-12 16:17:43 +0100

    Read user input as integer and determine if it is a positive number

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,16 @@
-with Ada.Text_IO;
-procedure Main is
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
+procedure Main is
+   N : Integer;
 begin
-   Ada.Text_IO.Put_Line("Hello, world");
+   Put("Enter an integer value: ");
+   Get(N);
+   if N > 0 then
+      Put(N);
+      Put_Line(" is a positive number");
+   else
+      Put(N);
+      Put_Line(" is a negative number");
+   end if;
 end Main;
